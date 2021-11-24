ae268c0a3040a1c1cd20d3a5ff6c460a9808ba13
Shahier Nashaat <shahiern@yahoo.com>
2021-11-24 11:25:56 +0200

    Make the calclulator logic work

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,36 +3,54 @@
 
 import './Calculator.css';
 import React from 'react';
+import calculate from './logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { total: '0' };
+    this.obj = {};
+  }
+
+  handleButtonClick = (e) => {
+    this.obj = calculate(this.obj, e.target.textContent);
+
+    if (this.obj.operation && !this.obj.next) {
+      this.setState({ total: this.obj.operation });
+    } else if (this.obj.next) {
+      this.setState({ total: this.obj.next });
+    } else if (this.obj.total) {
+      this.setState({ total: this.obj.total });
+    } else {
+      this.setState({ total: 0 });
+    }
   }
 
   render() {
+    const { total } = this.state;
     return (
       <div className="calculator">
-        <div className="result">0</div>
+        <div className="result">{total}</div>
         <div className="numbers-oberations">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="last">&divide;</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="last">&times;</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="last">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="last">+</button>
-          <button type="button" className="zero-btn">0</button>
-          <button type="button">.</button>
-          <button type="button" className="last">=</button>
+          <button type="button" onClick={this.handleButtonClick}>AC</button>
+          <button type="button" onClick={this.handleButtonClick}>+/-</button>
+          <button type="button" onClick={this.handleButtonClick}>%</button>
+          <button type="button" className="last" onClick={this.handleButtonClick}>&divide;</button>
+          <button type="button" onClick={this.handleButtonClick}>7</button>
+          <button type="button" onClick={this.handleButtonClick}>8</button>
+          <button type="button" onClick={this.handleButtonClick}>9</button>
+          <button type="button" className="last" onClick={this.handleButtonClick}>x</button>
+          <button type="button" onClick={this.handleButtonClick}>4</button>
+          <button type="button" onClick={this.handleButtonClick}>5</button>
+          <button type="button" onClick={this.handleButtonClick}>6</button>
+          <button type="button" className="last" onClick={this.handleButtonClick}>-</button>
+          <button type="button" onClick={this.handleButtonClick}>1</button>
+          <button type="button" onClick={this.handleButtonClick}>2</button>
+          <button type="button" onClick={this.handleButtonClick}>3</button>
+          <button type="button" className="last" onClick={this.handleButtonClick}>+</button>
+          <button type="button" className="zero-btn" onClick={this.handleButtonClick}>0</button>
+          <button type="button" onClick={this.handleButtonClick}>.</button>
+          <button type="button" className="last" onClick={this.handleButtonClick}>=</button>
         </div>
       </div>
     );
