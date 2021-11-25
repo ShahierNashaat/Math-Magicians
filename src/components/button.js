e5541e0b1f4a6a78d81b2c7c78b21acfd939f3c1
Shahier Nashaat <shahiern@yahoo.com>
2021-11-25 10:02:05 +0200

    Refactoring class components to functional

--- src/components/button.js
+++ src/components/button.js
@@ -1,24 +1,19 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable no-useless-constructor */
 /* eslint-disable react/prop-types */
 
 import React from 'react';
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-  }
-
-  handleButtonClick(e) {
-    const { handleButtonClick } = this.props;
+const Button = (props) => {
+  const handleClick = (e) => {
+    const { handleButtonClick } = props;
     handleButtonClick(e.target.textContent);
-  }
+  };
 
-  render() {
-    const { className, buttonName } = this.props;
-    return <button type="button" className={className} onClick={this.handleButtonClick}>{buttonName}</button>;
-  }
-}
+  const { className, buttonName } = props;
+  return (
+    <button type="button" className={className} onClick={handleClick}>
+      {buttonName}
+    </button>
+  );
+};
 
-export default Button;
+export { Button as default };
--- src/components/result.js
+++ src/components/result.js
@@ -1,18 +1,10 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable no-useless-constructor */
 /* eslint-disable react/prop-types */
 
 import React from 'react';
 
-class Result extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Result = (props) => {
+  const { output } = props;
+  return <div className="result">{output}</div>;
+};
 
-  render() {
-    const { output } = this.props;
-    return <div className="result">{output}</div>;
-  }
-}
-
-export default Result;
+export { Result as default };
