745e1e010f1a755bba6d6dce2d7c5fab607f181b
Shahier Nashaat <shahiern@yahoo.com>
2021-11-30 15:32:35 +0200

    Adding routes and styles

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -30,28 +30,31 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator">
-      <Result output={output} />
-      <div className="numbers-oberations">
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="AC" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="+/-" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="%" />
-        <Button handleButtonClick={handleButtonClick} className="last" buttonName="÷" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="7" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="8" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="9" />
-        <Button handleButtonClick={handleButtonClick} className="last" buttonName="x" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="4" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="5" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="6" />
-        <Button handleButtonClick={handleButtonClick} className="last" buttonName="-" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="1" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="2" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="3" />
-        <Button handleButtonClick={handleButtonClick} className="last" buttonName="+" />
-        <Button handleButtonClick={handleButtonClick} className="zero-btn" buttonName="0" />
-        <Button handleButtonClick={handleButtonClick} className="" buttonName="." />
-        <Button handleButtonClick={handleButtonClick} className="last" buttonName="=" />
+    <div className="container">
+      <h2>Let&apos;s do some math!</h2>
+      <div className="calculator">
+        <Result output={output} />
+        <div className="numbers-oberations">
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="AC" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="+/-" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="%" />
+          <Button handleButtonClick={handleButtonClick} className="last" buttonName="÷" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="7" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="8" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="9" />
+          <Button handleButtonClick={handleButtonClick} className="last" buttonName="x" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="4" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="5" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="6" />
+          <Button handleButtonClick={handleButtonClick} className="last" buttonName="-" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="1" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="2" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="3" />
+          <Button handleButtonClick={handleButtonClick} className="last" buttonName="+" />
+          <Button handleButtonClick={handleButtonClick} className="zero-btn" buttonName="0" />
+          <Button handleButtonClick={handleButtonClick} className="" buttonName="." />
+          <Button handleButtonClick={handleButtonClick} className="last" buttonName="=" />
+        </div>
       </div>
     </div>
   );
